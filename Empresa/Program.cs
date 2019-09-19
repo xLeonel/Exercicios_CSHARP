bdf3767471895dbc40b79213e956aea9cea55423
Vitor Leonel <vitorleonel100@hotmail.com>
2019-09-19 17:16:15 -0300

    second

---
+++ Empresa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Empresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double reajuste = 30.0/100.0;
+            double salario;
+            
+            Console.WriteLine("Qual é seu salário?");
+            salario = double.Parse(Console.ReadLine());
+
+            if (salario < 500.0){
+                salario = salario + ( salario * reajuste );
+                Console.WriteLine("Parabéns, você tem um reajuste disponível!");
+                Console.WriteLine("Saldo atual: " + salario);
+            } else {
+                Console.WriteLine("Ops, infelizmente você não tem reajuste.");
+            }
+        }
+    }
+}

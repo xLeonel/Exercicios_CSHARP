04e1926d0dd0efd1c410e6b0f59133378eff0629
Vitor Leonel <vitorleonel100@hotmail.com>
2019-09-19 16:06:50 -0300

    first

---
+++ Calculadora/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variaveis//
+
+            double num1;
+            double num2;
+            string oper;
+
+            //fim varaiveis//
+
+            Console.WriteLine("Digite o 1º número:");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º número");
+            num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o operador:");
+            oper = Console.ReadLine();
+
+            switch(oper) {
+                case "+": 
+                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                break;
+
+                case "-": 
+                Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                break;
+
+                case "*": 
+                Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                break;
+
+                case "/": 
+                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                break;
+
+                case "%": 
+                Console.WriteLine($"{num1} % {num2} = {num1 % num2}");
+                break;
+
+                default:  
+                Console.WriteLine("Operção Inválida!");
+                Console.WriteLine("Use: + , - , * , / ou %");
+                break;
+            }
+        }
+    }
+}
---
+++ Media/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1,nota2,nota3,nota4, media;
+
+
+            Console.Write("Digite sua Nota 1: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua Nota 2: ");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua Nota 3: ");
+            nota3 = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua Nota 4: ");
+            nota4 = double.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4 ; 
+
+            Console.WriteLine("Sua média é: " + media);
+
+            if(media >= 7) {
+                Console.WriteLine("Você está Aprovado");
+            } else {
+                Console.WriteLine("Você está Reprovado");
+            }
+
+        }
+    }
+}
